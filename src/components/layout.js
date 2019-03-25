aefa98f505ef46909ead35d67af5e202f1f6c38a
Christian Belisle <chris.belisle00@gmail.com>
2019-03-24 21:05:08 -0400

    Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Navbar from "../components/navbar"
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
@@ -13,7 +14,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
+            ...scale(1),
             marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
@@ -60,6 +61,7 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
+        <Navbar />
         <header>{header}</header>
         <main>{children}</main>
         <footer>
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./navbar.css"
+
+function Navbar() {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            |
+            <a href="/uses">Uses</a>
+        </nav>
+    )
+}
+
+export default Navbar
